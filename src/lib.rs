25c22dff8861413e12168550709b0a80ecbb8fde
vasiliy.andricov <vasiliy.andricov@south.rt.ru>
2021-03-16 14:07:57 +0300

    ч 3.

--- src/lib.rs
+++ src/lib.rs
@@ -9,8 +9,14 @@ use rocket_contrib::serve::StaticFiles;
 mod routes;
 
 pub fn rocket_builder() -> rocket::Rocket {
-    rocket::ignite()
-        .attach(SpaceHelmet::default())
-        .mount("/", routes![routes::echo::echo_fn])
-        .mount("/files", StaticFiles::from("static/"))
+    rocket::ignite().attach(SpaceHelmet::default())
+    .mount("/", routes![routes::ping::ping_fn])
+    .mount("/api", routes![
+        routes::user::user_list_rt,
+        routes::user::new_user_rt,
+        routes::user::info_user_rt,
+        routes::user::update_user_rt,
+        routes::user::delete_user_rt
+    ])
+    .mount("/files", StaticFiles::from("static/"))
 }
--- tests/basic_test.rs
+++ tests/basic_test.rs
@@ -1,11 +1,57 @@
-use rocket::local::Client;
-use rocket::http::Status;
-use rocket_tut::rocket_builder;
+use rocket::http::{ContentType, Status};
+use lazy_static;
+
+mod common;
 
 #[test]
-fn echo_test() {
-    let client = Client::new(rocket_builder()).expect("Valid Rocket instance");
-    let mut response = client.get("/echo/test_echo").dispatch();
+fn ping_test() {
+    let client = common::setup();
+    let mut response = client.get("/ping").dispatch();
     assert_eq!(response.status(), Status::Ok);
-    assert_eq!(response.body_string(), Some("test_echo".into()));
+    assert_eq!(response.body_string(), Some("PONG!".into()));
+}
+
+#[test]
+fn user_list_rt_test(){
+    let client = common::setup();
+    let mut response = client.get("/api/users").dispatch();
+    assert_eq!(response.status(), Status::Ok);
+    assert_eq!(response.content_type(), Some(ContentType::JSON));
+    assert_eq!(response.body_string(), Some("{\"status\":\"Success\",\"message\":\"List of users\"}".into()));
+}
+
+#[test]
+fn new_user_rt_test(){
+    let client = common::setup();
+    let mut response = client.post("/api/users").dispatch();
+    assert_eq!(response.status(), Status::Ok);
+    assert_eq!(response.content_type(), Some(ContentType::JSON));
+    assert_eq!(response.body_string(), Some("{\"status\":\"Success\",\"message\":\"Creation of new user\"}".into()));
+}
+
+#[test]
+fn info_user_rt_test(){
+    let client = common::setup();
+    let mut response = client.get("/api/users/1").dispatch();
+    assert_eq!(response.status(), Status::Ok);
+    assert_eq!(response.content_type(), Some(ContentType::JSON));
+    assert_eq!(response.body_string(), Some("{\"status\":\"Success\",\"message\":\"Info for user 1\"}".into()));
+}
+
+#[test]
+fn update_user_rt_test(){
+    let client = common::setup();
+    let mut response = client.put("/api/users/1").dispatch();
+    assert_eq!(response.status(), Status::Ok);
+    assert_eq!(response.content_type(), Some(ContentType::JSON));
+    assert_eq!(response.body_string(), Some("{\"status\":\"Success\",\"message\":\"Update info for user 1\"}".into()));
+}
+
+#[test]
+fn delete_user_rt_test(){
+    let client = common::setup();
+    let mut response = client.delete("/api/users/1").dispatch();
+    assert_eq!(response.status(), Status::Ok);
+    assert_eq!(response.content_type(), Some(ContentType::JSON));
+    assert_eq!(response.body_string(), Some("{\"status\":\"Success\",\"message\":\"Delete user 1\"}".into()));
 }
